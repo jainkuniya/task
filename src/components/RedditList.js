be3f5f10153a118dd6dce903d7ffe5e66699447c
Vishwesh <vishwesh03071998@gmail.com>
2017-04-08 16:44:52 +0530

    Added ListView.

--- src/components/RedditList.js
+++ src/components/RedditList.js
@@ -1,14 +1,21 @@
 //Import libraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ListView, Text } from 'react-native';
+
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
 //Create class component
 export default class RedditList extends Component {
 
 	render() {
+		const { data } = this.props;
+		const dataSource = ds.cloneWithRows(data);
 		return (
 			<View>
-				<Text>Vishwesh</Text>
+				<ListView
+					dataSource={dataSource}
+					renderRow={(rowData) => <Text>Reddit Data Item</Text>}
+				/>
 			</View>
 		);
 	}
